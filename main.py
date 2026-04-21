f628abb83e59b3cab03916a9373ebc4e754d61ad
Fazlul001 <fazlul.faizal.802@my.csun.edu>
2026-04-20 23:01:55 -0700

    added base url response

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "API is running"}
+    return {"message": "Hello, Thank you for visiting, the root url, check out other API links, Made Possible by, Fazlul, Joshua, Moh, Eitan, Fotios"}
 
 # Create an endpoint that listens for GET requests
 @app.get("/api/games")
 def get_games():
     # When someone hits this URL, send back this JSON data
     return {
-        "store": "Mock GameStop",
+        "store": "Gamestart an E-commerce, Online platform for Game enthusiasts",
         "inventory": ["Super Mario Odyssey", "Elden Ring", "Cyberpunk 2077"]
     }
