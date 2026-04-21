d57cab4a0b52ab302b82f54aeba68d981484bda1
Fazlul001 <fazlul.faizal.802@my.csun.edu>
2026-04-20 22:26:54 -0700

    new push

---
+++ api.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+# Initialize your API app
+app = FastAPI()
+
+# Create an endpoint that listens for GET requests
+@app.get("/api/games")
+def get_games():
+    # When someone hits this URL, send back this JSON data
+    return {
+        "store": "Mock GameStop",
+        "inventory": ["Super Mario Odyssey", "Elden Ring", "Cyberpunk 2077"]
+    }
